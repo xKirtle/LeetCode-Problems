fca05597b0e2eaa703db70ac681984f5bec40a21
Kirtle <xkirtle26@gmail.com>
2022-07-22 16:11:06 +0100

    15. Three Sum (not solved)

---
+++ Problems/ThreeSum.cs
@@ -0,0 +1,11 @@
+namespace LeetCode.Problems;
+
+public class ThreeSum : BaseProblem
+{
+    public ThreeSum() => SetProblemStats(15, "Three Sum", Difficulty.Medium);
+
+    protected override void ActualExecuteTest()
+    {
+        throw new NotImplementedException();
+    }
+}
